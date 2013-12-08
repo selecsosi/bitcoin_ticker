6f67f9b77224dd1772fcd73bd6ceea811280d07f
Sam Lahti <sam@gopangea.com>
2013-12-07 20:27:35 -0600

    trying dynamic ticker endpoint

--- ticker/urls.py
+++ ticker/urls.py
@@ -9,7 +9,7 @@ v1_api.register(QuoteTypeResource())
 v1_api.register(QuoteResource())
 
 urlpatterns = patterns('',
-    url(r'^quotes/mtgox/', 'ticker.views.quotes'),
+    url(r'^quotes/(?P<exchange_name>\w+)/', 'ticker.views.quotes'),
     url(r'^api/doc/', include('tastypie_swagger.urls', namespace='tastypie_swagger')),
     url(r'^api/', include(v1_api.urls)),
 
--- ticker/views.py
+++ ticker/views.py
@@ -7,23 +7,32 @@ from ticker.models import Quote, Exchange
 from django.views.decorators.cache import cache_page
 
 @cache_page(60)
-def quotes(request):
-    e = Exchange.objects.get(name="MtGox")
+def quotes(request, exchange_name="mtgox"):
+    try:
+        e = Exchange.objects.get(name__iexact=exchange_name)
+    except Exchange.DoesNotExist:
+        e = Exchange.objects.get(name__iexact="MtGox")
+
     ql_sell = Quote.objects.filter(quote_type__name="sell", exchange_endpoint__exchange=e).order_by('-modified')[:2000]
     ql_buy = Quote.objects.filter(quote_type__name="buy", exchange_endpoint__exchange=e).order_by('-modified')[:2000]
+    ql_last = Quote.objects.filter(quote_type__name="last", exchange_endpoint__exchange=e).order_by('-modified')[:2000]
 
-    x_sell_data = [calendar.timegm(x.modified.timetuple()) * 1000 for x in ql_sell]
-    ydata1 = [float(x.price) for x in ql_sell]
-    ydata2 = [float(x.price) for x in ql_buy]
+    x_sell = [calendar.timegm(x.modified.timetuple()) * 1000 for x in ql_sell]
+    x_buy = [calendar.timegm(x.modified.timetuple()) * 1000 for x in ql_buy]
+    x_last = [calendar.timegm(x.modified.timetuple()) * 1000 for x in ql_last]
+    y_sell = [float(x.price) for x in ql_sell]
+    y_buy = [float(x.price) for x in ql_buy]
+    y_last = [float(x.price) for x in ql_last]
 
-    tooltip_date = "%d %b %Y %H:%M:%S %p"
-    extra_serie = {"tooltip": {"y_start": ql_sell[0].to_currency.symbol, "y_end": ""},
-                    "date_format": tooltip_date}
+    # tooltip_date = "%d %b %Y %H:%M:%S %p"
+    # extra_serie = {"tooltip": {"y_start": ql_sell[0].to_currency.symbol, "y_end": ""},
+    #                 "date_format": tooltip_date}
 
     chartdata = {
-        'x': x_sell_data,
-        'name1': 'sell', 'y1': ydata1, 'extra1': extra_serie,
-        'name2': 'buy', 'y2': ydata2, 'extra2': extra_serie,
+        'name1': 'sell', 'x1': x_sell, 'y1': y_sell,
+        'name2': 'buy', 'x2': x_buy, 'y2': y_buy,
+        'name3': 'buy', 'x3': x_last, 'y3': y_buy,
+
     }
 
     charttype = "lineWithFocusChart"
